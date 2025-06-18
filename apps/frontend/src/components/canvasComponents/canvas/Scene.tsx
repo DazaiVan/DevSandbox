cb736cad4f69cf02ed0c7f6d4de09adfbf22987f
Иван Пирогов <iapirogov@istokmw.ru>
2025-06-18 17:23:45 +0300

    feat(GPU react)!: Изменение WebGL на WebGPU
    Изменение WebGL на WebGPU
    
    BREAKING CHANGE: Изменение WebGL на WebGPU

--- apps/frontend/src/components/canvasComponents/canvas/Scene.tsx
+++ apps/frontend/src/components/canvasComponents/canvas/Scene.tsx
@@ -1,17 +1,24 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, extend} from "@react-three/fiber";
 import Settings from "./Settings/Settings";
 import TestCube from "./auxiliaryModel/TestCube.jsx";
 import LoaderGLBModel from "./Loader/LoaderGLBModel.js";
 import { useGLTF ,AdaptiveDpr  } from '@react-three/drei';
 import { Suspense } from 'react';
 import { LoadingIndicator } from "./Settings/LoadingIndicator.js";
+import * as THREE from 'three/webgpu'
 interface SceneProps{
 
 }
 useGLTF.preload('/emo_robot.glb')
 function Scene({ }:SceneProps) {
   return (
-    <Canvas camera={{ far: 5000 }}>
+    <Canvas
+    gl={(props) => {
+      extend(THREE)
+      const renderer = new THREE.WebGPURenderer(props)
+      return renderer.init().then(() => renderer)
+    }}
+      camera={{ far: 5000 }}>
       <Suspense fallback={<LoadingIndicator />}>
             <Settings />
       <LoaderGLBModel url={'/emo_robot.glb'} />
