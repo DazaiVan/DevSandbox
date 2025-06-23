ff56010c148c4e3febcd4ca4771d977d2eb2bd17
Иван Пирогов <144069488+DazaiVan@users.noreply.github.com>
2025-06-23 17:07:14 +0300

    gpu init

--- apps/frontend/src/components/canvasComponents/canvas/Scene.tsx
+++ apps/frontend/src/components/canvasComponents/canvas/Scene.tsx
@@ -1,31 +1,36 @@
-import { Canvas, extend} from "@react-three/fiber";
+import { useRef } from 'react';
+import { Canvas, extend, ThreeToJSXElements } from "@react-three/fiber";
 import Settings from "./Settings/Settings";
 import TestCube from "./auxiliaryModel/TestCube.jsx";
 import LoaderGLBModel from "./Loader/LoaderGLBModel.js";
-import { useGLTF ,AdaptiveDpr  } from '@react-three/drei';
+import { useGLTF, AdaptiveDpr } from '@react-three/drei';
 import { Suspense } from 'react';
 import { LoadingIndicator } from "./Settings/LoadingIndicator.js";
-import * as THREE from 'three/webgpu'
-interface SceneProps{
-
+import * as THREE from 'three/webgpu';
+import * as TSL from 'three/tsl'
+interface SceneProps {}
+declare module '@react-three/fiber' {
+  interface ThreeElements extends ThreeToJSXElements<typeof THREE> {}
 }
-useGLTF.preload('/emo_robot.glb')
-function Scene({ }:SceneProps) {
+useGLTF.preload('/scene.glb');
+extend(THREE as any)
+function Scene({}: SceneProps) {
+
+
   return (
     <Canvas
-    gl={(props) => {
-      extend(THREE)
-      const renderer = new THREE.WebGPURenderer(props)
-      return renderer.init().then(() => renderer)
+    gl={async (props) => {
+      const renderer = new THREE.WebGPURenderer(props as any)
+      await renderer.init()
+      return renderer
     }}
-      camera={{ far: 5000 }}>
+      camera={{ far: 5000 }}
+    >
       <Suspense fallback={<LoadingIndicator />}>
-            <Settings />
-      <LoaderGLBModel url={'/emo_robot.glb'} />
-        {/* <TestCube /> */}
+        <Settings />
+        <LoaderGLBModel url={'/scene.glb'} />
         <AdaptiveDpr pixelated />
       </Suspense>
-
     </Canvas>
   );
 }
